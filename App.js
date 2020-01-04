7ae8b900158ad9817e11f1cd0655a53008f8fef9
Adeilson <adeilson@gmail.com>
2020-01-04 09:05:04 -0300

    - Adding camera component

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Text, View} from 'react-native';
+import Cam from './src/components/Cam';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -22,8 +8,9 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={{flex: 1}}>
+          <Cam />
+        </View>
       </View>
     );
   }
@@ -40,10 +27,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
